2cd04a4aedcd84fb8fc25ca78d8e02c96ef85af6
crapu <crapu@example.com>
2026-03-06 16:18:33 +0200

    router, views

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,43 +1,17 @@
-const btn = document.querySelector('button') as HTMLButtonElement;
-const userDiv = document.getElementById('user') as HTMLDivElement;
-const nameDiv = document.getElementById('name') as HTMLDivElement;
-const ageDiv = document.getElementById('age') as HTMLDivElement;
+import { subscriber, createSignal, createEffect } from './helpers/Signal.js';
+import { router, navigateTo } from './helpers/Router.js';
 
-let subscriber: Function | null = null;
+// const app = document.getElementById('app') as HTMLDivElement;
+// const loginBtn = document.querySelector('button') as HTMLButtonElement;
 
-const createSignal = <T>(initialValue: T): [() => T, (value: T) => void] => {
-  let _value = initialValue;
-  const _subscriptions = new Set<Function>();
+// const [state, setState] = createSignal(0);
 
-  const read = () => {
-    if (subscriber && !_subscriptions.has(subscriber)) {
-      _subscriptions.add(subscriber);
-    }
-    return _value;
-  };
-  const write = (newValue: T): void => {
-    if (newValue === _value) return;
-    _value = newValue;
-    _subscriptions.forEach((cb) => cb());
-  };
-  return [read, write];
-};
+// createEffect(() => console.log(state()));
+// createEffect(() => {
+//   let out = state();
+//   app.innerHTML = `${out} -- ${out * 2} -- ${out * 10}`;
+// });
 
-const createEffect = (cb: Function) => {
-  subscriber = cb;
-  cb();
-  subscriber = null;
-};
-
-const [state, setState] = createSignal(0);
-
-createEffect(() => console.log(state()));
-createEffect(() => {
-  userDiv.innerHTML = `${state()}`;
-  nameDiv.innerHTML = `${state() * 2}`;
-  ageDiv.innerHTML = `${state() * 10}`;
-});
-
-btn.addEventListener('click', () => {
-  setState(state() + 1);
-});
+// loginBtn.addEventListener('click', () => {
+//   setState(state() + 1);
+// });
